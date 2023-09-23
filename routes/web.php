7e9a0b065f5c9b5b28de49c4571ff7323643e538
Faroeq <alves_1044@hotmail.com>
2023-09-23 21:50:31 +0200

    Add Tailwind Css library

--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/product', [ProductController::class, 'show']);
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('app');
 });
