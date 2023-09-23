47a503e596bcbc9fa7ab7aec7242b4261e547357
Faroeq <alves_1044@hotmail.com>
2023-09-23 17:39:16 +0200

    Add productseeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,13 +5,17 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
+class DatabaseSeeder extends Seeder {
     /**
      * Seed the application's database.
      */
-    public function run(): void
-    {
+    public function run(): void {
+        // Call methode maakt mogelijk dat je meerdere seeders of factories kan toevoegen
+        $this->call([
+            // In de toekomst wil ik nog meerdere seeders toevoegen,
+            ProductSeeder::class
+        ]);
+
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
